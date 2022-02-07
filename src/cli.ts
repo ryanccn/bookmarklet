316bcced95373aa4a83b33173543c536a4ec9532
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-02-07 21:59:13 +0800

    v0.0.4

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,7 @@ import sade from 'sade';
 import { build } from '/@/commands/build';
 
 sade('bookmarklet')
-  .version('0.0.3')
+  .version('0.0.4')
   .command('build <file>')
   .describe('Build a bookmarklet')
   .option('--write', 'The file to write to')
